661e61e5ddbcb339d10ce33f42ec89ec4381516f
Denise Hermoza <denise.hermoza@pucp.pe>
2021-12-01 00:00:54 -0500

    se agregaron comentarios

--- challenge.rb
+++ challenge.rb
@@ -2,19 +2,20 @@
 
 def balance(string)
     array = []
-    words = string.split("")
-    only_strings = string.gsub(/[)(]/, '')
-    words.each_with_index do |word, index|
-      if word == "(" && words[index-1] != ":" && only_strings.length > 1
+    words = string.split("") # guardamos en un array words todos los caracteres de la frase a comprobar
+    only_strings = string.gsub(/[)(]/, '') # guardamos en otro string sólo los caracteres sin parentesis
+    words.each_with_index do |word, index| # recorremos el array words
+    if word == "(" && words[index-1] != ":" && only_strings.length > 1 #solo agregamos un parentesis al array word si no es parte de un emoticon para
+      # o si el string sin parentesis contiene mas de un caracter
         array.push(word)
-      elsif word == ")" && words[index-1] != ":"
+      elsif word == ")" && words[index-1] != ":" #solo agregamos un parentesis al array si no es emoticon
         array.push(word)
       end
     end
-    if array.length.odd?
+    if array.length.odd? # es desbalanceado si el tamaño del array de parentesis es impar. No se incluyeron los parentesis que eran parte de un emoticon
       return "desbalanceado"
     end
-  return "balanceado"
+  return "balanceado" #es balanceado si el tamaño del array de parentensis es par
 end
 
 #def balance2(string)
