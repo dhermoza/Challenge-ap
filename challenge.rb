5aa820178e97b1013aeae4679fe7aabac212bf08
Denise Hermoza <denise.hermoza@pucp.pe>
2021-11-30 23:56:59 -0500

    challenge completed

--- challenge.rb
+++ challenge.rb
@@ -1,23 +1,38 @@
 #Programa para definir si un mensaje tiene los parentesis desbalanceados o no
 
 def balance(string)
- if string.empty? || string.length >= 1 && string.match?(/:\(|:\)|\([a-zA-Z:\s]+\)|[a-zA-Z:\s]+|:\(|:\)/i)
+    array = []
+    words = string.split("")
+    only_strings = string.gsub(/[)(]/, '')
+    words.each_with_index do |word, index|
+      if word == "(" && words[index-1] != ":" && only_strings.length > 1
+        array.push(word)
+      elsif word == ")" && words[index-1] != ":"
+        array.push(word)
+      end
+    end
+    if array.length.odd?
+      return "desbalanceado"
+    end
   return "balanceado"
- else
-  return "desbalanceado"
- end
 end
 
-#puts balance("hola") #balanceado
-#puts balance("(hola)") #balanceado
-#puts balance("(()") #desbalanceado
-#puts balance("(:)") #balanceado
-#puts balance("no voy (:()") #balanceado
-#puts balance("hoy pm: fiesta :):)") #balanceado
-#puts balance("(hola :)") #desbalanceado
-#puts balance("hola :((")
-#puts balance("a (b (c (d) c) b) a :)") #balanceado
+#def balance2(string)
+ # if string.empty? || string.length >= 1 && string.match?(/:\(|:\)|\([a-zA-Z:\s]+\)|[a-zA-Z:\s]+|:\(|:\)/i)
+ #   return "balanceado"
+ # end
+#end
+puts balance("hola") #balanceado
+puts balance("(hola)") #balanceado
+puts balance("(()") #desbalanceado
+puts balance("(:)") #balanceado
+puts balance(":)") #balanceado !!
+puts balance("no voy (:()") #balanceado
+puts balance("hoy pm: fiesta :):)") #balanceado
+puts balance("(hola :)") #desbalanceado
+puts balance("a (b (c (d) c) b) a :)") #balanceado
 
+#Casos de Prueba
 #a. "hola" -> balanceado
 #b. "(hola)" -> balanceado
 #c. "(()" -> desbalanceado
